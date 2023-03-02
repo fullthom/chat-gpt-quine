d574f39571540bbee275bc01014f105171caa2b8
Thomas Fuller <thomasfuller98@gmail.com>
2023-03-01 21:25:50 -0800

    Update prompt and response parsing

--- run.py
+++ run.py
@@ -20,12 +20,6 @@ logger.addHandler(stream_handler)
 
 API_KEY = ""
 
-START_PROMPT = """
-Can you write some python code that does something? You can be creative in what it does.
-"""
-
-EXTRACT_CODE_PATTERN = r"```\n([\s\S]+?)```"
-
 
 # @@@
 
@@ -40,6 +34,7 @@ def create_chat_completion(prompt, model="gpt-3.5-turbo"):
     response = requests.post(url, headers=headers, json=data)
     logger.debug("End of create_chat_completion")
     if response.status_code == 200:
+        logger.debug("API Response: " + json.dumps(response))
         return response.json()
     else:
         msg = "Request to API failed!"
@@ -67,18 +62,15 @@ def get_code_from_completion_response(response: dict) -> str:
     logger.debug("Full Response Content: " + content)
 
     # Extract the first code from the response
-    code = ""
-    for match in re.finditer(EXTRACT_CODE_PATTERN, content):
-        code = match.group(1).strip()
-        logger.debug("Extracted code: " + code)
-        if code.startswith("python"):
-            code = code[6:].strip()
-        break
-
-    if not code:
-        logger.error("No code found in response")
-        raise ValueError("No code found in response")
-
+    re_res = re.findall(r"```\n([\s\S]+?)```", content)
+    logger.debug(re_res)
+    if len(re_res) > 0:
+        code = re_res[0]
+    else:
+        # Maybe the entire response is code?
+        code = content
+    code = code.removeprefix("```").removeprefix("python").removesuffix("```")
+    logger.info("Extracted Code: " + code)
     return code
 
 
@@ -88,9 +80,7 @@ def run():
     prompt = read_prompt()
     logger.debug("Using prompt: ")
     res = create_chat_completion(prompt)
-    logger.debug("API Response: " + json.dumps(res))
     code = get_code_from_completion_response(res)
-    logger.info("Extracted Code: " + code)
 
     logger.info("Writing to new.py")
     with open("run.py", "r") as f:
@@ -102,6 +92,5 @@ def run():
 
 
 if __name__ == "__main__":
-    write_prompt(START_PROMPT)
     run()
     restart_program()
